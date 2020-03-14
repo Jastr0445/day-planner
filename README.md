04135c68b029d9f138c2d96ed78d22f7306f317e
Jackson Strauss <58577918+Vaultboy58@users.noreply.github.com>
2020-03-14 01:32:35 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # day-planner
 homework
+make a var for the colors 
+
+
+
+
---
+++ hours.js
@@ -0,0 +1,39 @@
+
+var colors;
+
+ 
+
+var hours; 
+for (i = hour; 9 < days.length; i++) { 
+  text += hours[i] + "<br>";
+}
+
+for 
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
