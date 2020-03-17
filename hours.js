97ec49f5f9b07f4d31cbc91fbf245da659aa41a1
Jackson Strauss <58577918+Vaultboy58@users.noreply.github.com>
2020-03-17 01:14:34 -0500

    Add files via upload

---
+++ hours.js
@@ -0,0 +1,41 @@
+
+var colors;
+
+ 
+
+var hours; 
+for (i = hour; 9 < days.length; i++) { 
+  text += hours[i] + "<br>";
+
+  console.log('loop' )
+
+}
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
